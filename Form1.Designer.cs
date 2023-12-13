089136546b6eaf5fdd5ca9347006eaa237700c7b
LunaroakF <smallkay063@outlook.com>
2023-12-13 18:19:25 +0800

    1.2.2.0

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -255,7 +255,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Nepeta cataria";
+            this.Text = "Nepeta cataria v1.2.2";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
