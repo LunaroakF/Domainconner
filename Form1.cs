460b23550e0a1c3b871ac669c2afbe540162797d
LunaroakF <smallkay063@outlook.com>
2023-12-14 10:10:00 +0800

    请求管理员权限运行记事本

--- Form1.cs
+++ Form1.cs
@@ -250,30 +250,12 @@ namespace Domainconner
                 //reaaddbutton.Text = "修改";
             }
         }
-        public void CmdLine(string str)//调用cmd命令行
-        {
-            try
-            {
-                using (Process process = new Process())
-                {
-                    process.StartInfo.FileName = "cmd.exe";//调用cmd.exe程序
-                    process.StartInfo.UseShellExecute = false;
-                    process.StartInfo.RedirectStandardInput = true;//重定向标准输入
-                    process.StartInfo.RedirectStandardOutput = true;//重定向标准输出
-                    process.StartInfo.RedirectStandardError = true;//重定向标准出错
-                    process.StartInfo.CreateNoWindow = true;//不显示黑窗口
-                    process.Start();//开始调用执行
-                    process.StandardInput.WriteLine(str + "&exit");//标准输入str + "&exit"，相等于在cmd黑窗口输入str + "&exit"
-                    process.StandardInput.AutoFlush = true;//刷新缓冲流，执行缓冲区的命令，相当于输入命令之后回车执行
-                    process.WaitForExit();//等待退出
-                    process.Close();//关闭进程
-                }
-            }
-            catch { }
-        }
         private void hostsButton_Click(object sender, EventArgs e)
         {
-            CmdLine("notepad C:\\Windows\\System32\\drivers\\etc\\hosts");
+            ProcessStartInfo info = new ProcessStartInfo(@"C:\Windows\Notepad.exe", @"C:\Windows\System32\drivers\etc\hosts");
+            info.UseShellExecute = true;
+            info.Verb = "runas";
+            Process.Start(info);
         }//点击编辑Hosts按钮
     }
 }
